d34ca1bf2bd9ecd9396fec56916d08831bf2f907
TokiLava <tokilava@gmail.com>
2023-04-23 14:18:42 +0200

    changed the html file to php, and included some code that establishes a connection to the server

--- Test1.php
+++ Test1.php
@@ -8,31 +8,31 @@ $conn = OpenCon();
 <head>
 	<title>My Website</title>
 	<link rel="stylesheet" href="Test1.css">
-	<meta charset="UTF-8">
 </head>
 <body>
     <div class="logo-container">
         <img src="LogoDark.png">
     </div>
-	<div>
-		<img id="drake" src="Drake-Hotline-Bling.jpg"/>
-	</div>
-	<div class="container">
-		<div class="left-container">
-			<h2>Top Text</h2>
-			<input type="text" placeholder="Enter top text here">
-			<h2>Bottom Text</h2>
-			<input type="text" placeholder="Enter bottom text here">
-                  <div class="controls">
-                      <button id="thisButton" class="generate-button" onclick="loadDoc()">Generate Meme</button>
-                      <button class="share-button">Share Meme</button>
-                  </div>
-		</div>
-		<div class="right-container">
-			<img src="https://via.placeholder.com/500x500" alt="Arbitrary Image" class="image">
-		</div>
-	</div>
-	<script src="data-html.js"></script>
+    <div class="container">
+        <h2>Top Text</h2>
+        <input type="text" placeholder="Enter text here">
+        <h2>Bottom Text</h2>
+        <input type="text" placeholder="Enter text here">
+        <img img id="placeholder" src="PlaceHolder.jpg">
+        <div class="button-container">
+            <button class="generate-button">Generate</button>
+            <button class="share-button">Share</button>
+        </div>
+    </div>
+    <div class="meme-container">
+        <h2>Choose meme template:</h2>
+          <div class="grid-container">
+              <div class="grid-item"><img src="Stramt.png"></div>
+              <div class="grid-item"><img src="chad.png"></div>
+              <div class="grid-item"><img src="pov.png"></div>
+              <div class="grid-item"><img src="Pog.png"></div>
+          </div>
+    </div>
 </body>
 </html>
 
