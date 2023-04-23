c132bab31e1ecec32b4b4ae50045027a1e1fa083
TokiLava <tokilava@gmail.com>
2023-04-23 20:09:57 +0200

    made a file called images, and moved all the pictures in there, and renamed all src to image/

--- Test1.php
+++ Test1.php
@@ -11,14 +11,14 @@ $conn = OpenCon();
 </head>
 <body>
     <div class="logo-container">
-        <img src="LogoDark.png">
+        <img src="images/LogoDark.png">
     </div>
     <div class="container">
         <h2>Top Text</h2>
         <input type="text" placeholder="Enter text here">
         <h2>Bottom Text</h2>
         <input type="text" placeholder="Enter text here">
-        <img img id="placeholder" src="PlaceHolder.jpg">
+        <img img id="placeholder" src="images/PlaceHolder.jpg">
         <div class="button-container">
             <button class="generate-button">Generate</button>
             <button class="share-button">Share</button>
@@ -27,10 +27,10 @@ $conn = OpenCon();
     <div class="meme-container">
         <h2>Choose meme template:</h2>
           <div class="grid-container">
-              <div class="grid-item"><img src="Stramt.png" data-image = "Stramt.png" alt = "Stramt.png"> </div> <!--we use the data-* attribute to store cutom data, and also add alt attribute if the image does not load -->
-              <div class="grid-item"><img src="chad.png" data-image = "chad.png" alt = "chad.png"> </div>
-              <div class="grid-item"><img src="pov.png" data-image = "pov.png" alt = "pov.png"> </div>
-              <div class="grid-item"><img src="Pog.png" data-image = "Pog.png" alt = "Pog.png"> </div>
+              <div class="grid-item"><img src="images/Stramt.png" data-image = "Stramt.png" alt = "Stramt.png"> </div> <!--we use the data-* attribute to store cutom data, and also add alt attribute if the image does not load -->
+              <div class="grid-item"><img src="images/chad.png" data-image = "chad.png" alt = "chad.png"> </div>
+              <div class="grid-item"><img src="images/pov.png" data-image = "pov.png" alt = "pov.png"> </div>
+              <div class="grid-item"><img src="images/Pog.png" data-image = "Pog.png" alt = "Pog.png"> </div>
           </div>
     </div>
 </body>
